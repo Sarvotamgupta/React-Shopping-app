7938fa724545be476ceb7e70122fe6ed469e80fc
Your Name <you@example.com>
2024-07-31 14:30:42 -0400

    updated the files to tsx

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import App from "./App.tsx";
+import reportWebVitals from "./reportWebVitals.ts";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
 
-reportWebVitals();
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+
+  reportWebVitals();
+} else {
+  console.error("Root element not found");
+}
